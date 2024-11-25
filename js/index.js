7dd2d70fa75360e5ff648f0bc6aae62e18a97dc4
Nikita Shah <nikita12.shah@gmail.com>
2024-11-25 17:01:57 +0530

    Adding changes for Iteration 2 - Conditionals.

--- js/index.js
+++ js/index.js
@@ -18,5 +18,26 @@ console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
 
+/* 
+2.1. Depending on which name is longer, print:
+      • The driver has the longest name, it has XX characters. or
+      • It seems that the navigator has the longest name, it has XX characters. or
+      • Wow, you both have equally long names, XX characters!.
+*/
+
+let hacker1Length = hacker1.length;
+let hacker2Length = hacker2.length;
+
+if (hacker1Length > hacker2Length) {
+    console.log(`The driver has the longest name, it has ${hacker1Length} characters.`);
+} else if (hacker1Length < hacker2Length) {
+    console.log(`It seems the navigator has the longest name, it has ${hacker2Length} characters.`);
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1Length} characters!`);
+}
+
+// ========== End of Iteration 2 ========== //
+
+
 
 // Iteration 3: Loops
