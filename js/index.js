d5e50e815a1837a0b7db9dd684553de3ca161b8f
Nikita Shah <nikita12.shah@gmail.com>
2024-11-25 18:05:46 +0530

    Adding changes for Iteration 3 - Loops.

--- js/index.js
+++ js/index.js
@@ -20,9 +20,9 @@ console.log(`The navigator's name is ${hacker2}.`);
 
 /* 
 2.1. Depending on which name is longer, print:
-      • The driver has the longest name, it has XX characters. or
-      • It seems that the navigator has the longest name, it has XX characters. or
-      • Wow, you both have equally long names, XX characters!.
+     1. The driver has the longest name, it has XX characters. or
+     2. It seems that the navigator has the longest name, it has XX characters. or
+     3. Wow, you both have equally long names, XX characters!.
 */
 
 let hacker1Length = hacker1.length;
@@ -41,3 +41,51 @@ if (hacker1Length > hacker2Length) {
 
 
 // Iteration 3: Loops
+
+/*
+3.1 Print all the characters of the driver's name, separated by a space and in capitals
+i.e. "J O H N"
+*/
+
+let driverName = "";
+
+for (let i = 0; i < hacker1Length; i++) {
+    if (hacker1.charAt(i) !== " ") {
+        driverName += hacker1.charAt(i).toUpperCase();
+        if (i < hacker1Length - 1) {
+            driverName += " ";
+        }
+    }
+}
+
+console.log(driverName);
+
+/*
+3.2 Print all the characters of the navigator's name, in reverse order.
+i.e. "nhoJ"
+*/
+
+let navigatorName = "";
+
+for (let j = hacker2Length -1; j >= 0; j--) {
+    navigatorName += hacker2[j];
+}
+
+console.log(navigatorName);
+
+/*
+3.3 Depending on the lexicographic order of the strings, print:
+    1. The driver's name goes first.
+    2. Yo, the navigator goes first definitely.
+    3. What?! You both have the same name?
+*/
+
+if (hacker1 > hacker2) {
+    console.log("The driver's name goes first.");
+} else if (hacker1 < hacker2) {
+    console.log("Yo, the navigator goes first definitely.")
+} else {
+    console.log("What?! You both have the same name?");
+}
+
+// ========== End of Iteration 3 ========== //
