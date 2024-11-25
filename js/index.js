e1d3fb6b24751756a53932a7ad460526087bbb37
Nikita Shah <nikita12.shah@gmail.com>
2024-11-25 16:45:14 +0530

    Adding changes for Iteration 1 - Names and Input.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 // Iteration 1: Names and Input
-//
+
+// 1.1 Create a variable hacker1 with the driver's name.
+let hacker1 = "Edward Cullen";
+
+// 1.2 Print "The driver's name is XXXX".
+console.log(`The driver's name is ${hacker1}.`);
+
+// 1.3 Create a variable hacker2 with the navigator's name.
+let hacker2 = "Jacob Black";
+
+// 1.4 Print "The navigator's name is YYYY".
+console.log(`The navigator's name is ${hacker2}.`);
+
+// ========== End of Iteration 1 ========== //
+
+
+
 // Iteration 2: Conditionals
 
 
